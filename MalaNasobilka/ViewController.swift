d7bed104616d822092858e74038892d85171041f
Michal Dzacovsky <admin@117-dhcp.lan.univers.sk>
2016-03-08 15:47:50 +0100

    1.update

--- MalaNasobilka/ViewController.swift
+++ MalaNasobilka/ViewController.swift
@@ -10,6 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Prva obrazovka
+    
+    @IBOutlet weak var BG1: UIImageView!
+    @IBOutlet weak var NasobilkaBnr: UIImageView!
+    @IBOutlet weak var ZadajCisloTxt: UITextField!
+    @IBOutlet weak var HrajBtn: UIButton!
+    
+    
+    // Druha Obrazovka
+    
+    @IBOutlet weak var BG2: UIImageView!
+    @IBOutlet weak var PocitanieTxt: UILabel!
+    @IBOutlet weak var SipkaDalej: UIButton!
+    
+    
+    // Samotny program
+    @IBAction func HrajProgram(sender: AnyObject) {
+    
+        NasobilkaBnr.hidden = true
+        ZadajCisloTxt.hidden = true
+        BG1.hidden = true
+        HrajBtn.hidden = true
+    
+        BG2.hidden = false
+        PocitanieTxt.hidden = false
+        SipkaDalej.hidden = false
+        
+        
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
